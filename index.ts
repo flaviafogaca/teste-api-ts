72ffaf43b04a20884e0076b7804a00201b2177fc
flaviafogaca <flaviamariafogaca@gmail.com>
2023-02-14 11:07:07 -0400

    update index

---
+++ index.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express'
+import axios from 'axios'
+
+const app = express()
+
+app.listen('3000')
+
+// 1º Endpoint
+app.route('/users').get( (req: Request, res: Response) => {
+    axios.get('https://mockend.com/juunegreiros/BE-test-api/users')
+    .then((result) => res.send(result.data))
+    .catch((error) => console.error(`error`))
+})
+
+// 2º Endpoint
+app.route('/products').get( (req: Request, res: Response) => {
+    axios.get('https://mockend.com/juunegreiros/BE-test-api/products')
+    .then((result) => res.send(result.data))
+    .catch((error) => console.error(`error`))
+})
+
+// 3º Endpoint (para evoluir e finalizar)
+app.route('/calculate').get( (req: Request, res: Response) => {
+    console.log(req);
+    res.send(`teste`)
+});
+
+axios
+    .get('http://localhost:3000/calculate?users=2&products=1&products=4&products=50')
+    .then(function (resposta) {
+    console.log(resposta.data) 
+});
